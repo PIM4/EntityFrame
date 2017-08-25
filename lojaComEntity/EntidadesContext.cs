1342564a739052a53b4631f6796c8ebe1f29bf8d
CarlosEscouto <carloshenriqueeescouto@gmail.com>
2017-08-25 12:01:02 -0300

    Teste Utilizando o Entity

---
+++ lojaComEntity/EntidadesContext.cs
@@ -0,0 +1,28 @@
+﻿using lojaComEntity.Entity;
+using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lojaComEntity
+{   
+    // Classe do Entity, que eu criei como EntidadesContext e ela herda de uma Classe "DbContext"
+    public class EntidadesContext : DbContext 
+    {
+        // Crio uma lista de usario do tipo Dbset com o nome Usuarios; 
+        public DbSet<Usuario>  Usuarios { get; set; }
+
+        // Um metodo que existe na classe pai, a "DbContext" e aqui eu estou usando o "override" para sobreescrever o metodo
+        // Com os dados que eu quero, no caso 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // String de conexão. Essa string esta dentro do "App.config", que mostra a configuração do banco de dados
+            string stringConexao = ConfigurationManager.ConnectionStrings["LojaConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(stringConexao );
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ lojaComEntity/Program.cs
@@ -0,0 +1,43 @@
+﻿using lojaComEntity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancia os objetos, tanto do Entity quanto o Usuario.
+            EntidadesContext contexto = new EntidadesContext();
+            Usuario user = new Usuario();
+
+            // Mostra mensagem na tela e pega a resposta, savlando isso no "Nome".
+            Console.WriteLine("Sistema de cadastro de usuario. \n");
+            Console.WriteLine("Digite seu nome: ");
+            user.Nome = Console.ReadLine();
+
+            // Pula linha \o/
+            Console.WriteLine("\n");
+
+            // Mostra mensagem na tela e pega a resposta, savlando isso no "Senha".
+            Console.WriteLine("Digite sua senha: ");
+            user.Senha = Console.ReadLine();
+
+            // Metodos do objeto "contexto" que foi criado na instancia do "EntidadesContext" que é a
+            // Classe que criei pro entity, lá no começo do codigo 
+            contexto.Usuarios.Add(user);
+            contexto.SaveChanges();
+            contexto.Dispose();
+
+            // Mensagem de sucesso e uma pausa, para a tela não fechar direito.
+            Console.WriteLine("Salvou o Usuario");
+            Console.ReadKey();
+        }   
+    }
+}
